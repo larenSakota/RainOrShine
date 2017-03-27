dd5c5bce41e925e91a886130094179f4f0588944
larenSakota <jedi.temple@yahoo.com>
2017-03-27 13:01:33 -0700

    setting up UI

---
+++ RainOrShine/WeatherVC.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherVC.swift
+//  RainOrShine
+//
+//  Created by Laren Sakota on 3/24/17.
+//  Copyright © 2017 3..2..1..GO. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+}
+
